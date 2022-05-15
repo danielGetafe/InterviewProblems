a0dcc98860ad0fcac7cd4e58801023031de4c384
danielGetafe <daniel.vieira.escudero@gmail.com>
2022-05-15 20:16:03 +0200

    findMedianSortedArrays first approach

--- MedianOf2SortedArrays/MedianOf2SortedArrays.py
+++ MedianOf2SortedArrays/MedianOf2SortedArrays.py
@@ -5,5 +5,43 @@ class Solution:
     nums2 --> List[int]
     return value --> float
     '''
-    def findMedianSortedArrays(self, nums1, nums2):
-        pass
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
+        
+        nums1Len, nums2Len = len(nums1), len(nums2)
+        maxLenMergedList = (nums1Len + nums2Len) / 2 + 0.5
+        index1, index2 = 0, 0
+        mergedList = []
+        done = False
+
+        if nums1Len + nums2Len == 0:
+            return 0
+
+        while len(mergedList) < maxLenMergedList:
+
+            if index1 < nums1Len and index2 < nums2Len:
+                if nums1[index1] <= nums2[index2]:
+                    mergedList.append(nums1[index1])
+                    index1 += 1
+                else:
+                    mergedList.append(nums2[index2])
+                    index2 += 1
+            elif index1 < nums1Len:
+                mergedList.append(nums1[index1])
+                index1 += 1
+            else:
+                mergedList.append(nums2[index2])
+                index2 += 1
+
+
+
+        if (nums1Len + nums2Len) % 2 == 0:
+            return (mergedList[-1] + mergedList[-2]) / 2
+        return mergedList[-1]
+
+def main():
+    a, b = [1, 3], [2]
+    solution = Solution()
+    print(solution.findMedianSortedArrays(a, b))
+
+if __name__ == "__main__":
+    main()
