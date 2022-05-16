b8031e954bb3c46b1cce15158fa5e8270f226acf
Daniel <daniel.vieira.escudero@gmail.com>
2022-05-16 20:33:54 +0200

    result storage removed for merged list

--- MedianOf2SortedArrays/MedianOf2SortedArrays.py
+++ MedianOf2SortedArrays/MedianOf2SortedArrays.py
@@ -1,6 +1,8 @@
 class Solution:
 
     '''
+    We assume that both nums1 and nums2
+    are numeric sorted lists
     nums1 --> List[int]
     nums2 --> List[int]
     return value --> float
@@ -8,36 +10,55 @@ class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
         
         nums1Len, nums2Len = len(nums1), len(nums2)
-        maxLenMergedList = (nums1Len + nums2Len) / 2 + 0.5
+        maxLenMergedList = (nums1Len + nums2Len) // 2 + 1
         index1, index2 = 0, 0
-        mergedList = []
-        done = False
+        lastNums1, lastNums2 = 0, 0
 
         if nums1Len + nums2Len == 0:
             return 0
 
-        while len(mergedList) < maxLenMergedList:
+        if (nums1Len + nums2Len) % 2 == 1: #Sum is odd
 
-            if index1 < nums1Len and index2 < nums2Len:
-                if nums1[index1] <= nums2[index2]:
-                    mergedList.append(nums1[index1])
+            while (index1 + index2) < maxLenMergedList:
+
+                if index1 < nums1Len and index2 < nums2Len:
+                    if nums1[index1] <= nums2[index2]:
+                        lastNums1 = nums1[index1]
+                        index1 += 1
+                    else:
+                        lastNums1 = nums2[index2]
+                        index2 += 1
+
+                elif index1 < nums1Len:
+                    lastNums1 = nums1[index1]
+                    index1 += 1
+
+                else:
+                    lastNums1 = nums2[index2]
+                    index2 += 1
+
+            return lastNums1
+
+        else: #Sum is even
+
+            while (index1 + index2) < maxLenMergedList:
+                if index1 < nums1Len and index2 < nums2Len:
+                    if nums1[index1] <= nums2[index2]:
+                        lastNums1 = nums1[index1]
+                        index1 += 1
+                    else:
+                        lastNums2 = nums2[index2]
+                        index2 += 1
+                elif index1 < nums1Len:
+                    lastNums1 = nums1[index1]
                     index1 += 1
                 else:
-                    mergedList.append(nums2[index2])
+                    lastNums2 = nums2[index2]
                     index2 += 1
-            elif index1 < nums1Len:
-                mergedList.append(nums1[index1])
-                index1 += 1
-            else:
-                mergedList.append(nums2[index2])
-                index2 += 1
 
+            return (lastNums1 + lastNums2) / 2
 
-
-        if (nums1Len + nums2Len) % 2 == 0:
-            return (mergedList[-1] + mergedList[-2]) / 2
-        return mergedList[-1]
-
+'''Tests examples'''
 def main():
     a, b = [1, 3], [2]
     solution = Solution()
