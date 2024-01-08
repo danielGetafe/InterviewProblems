8a7a5fdf3525a52c61418cc1d3af5dd5866292af
Daniel <daniel.vieira.escudero@gmail.com>
2024-01-08 22:55:11 +0100

    valid palindrome

---
+++ valid_palindrome/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(ch for ch in s if ch.isalnum())
+        for index in range(int((len(s)/2))):
+            if s[index] != s[-(index+1)]:
+                return False
+        return True
+    
+
+if __name__ == "__main__":
+    s = "race a car"
+    assert Solution().isPalindrome(s) is False
+    s = "A man, a plan, a canal: Panama"
+    assert Solution().isPalindrome(s)
+    s = " "
+    assert Solution().isPalindrome(s)
+    print("All tests passed!")
